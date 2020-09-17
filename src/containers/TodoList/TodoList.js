fd6c4fe2cd98eb98cea7794602795107fb8ebdc6
mushypeas <qkrwoans2@gmail.com>
2020-09-17 20:22:45 +0900

    commit

---
+++ src/containers/TodoList/TodoList.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Todo from "../../components/Todo/Todo";
+import './TodoList.css';
+import TodoDetail from "../../components/TodoDetail/TodoDetail";
+import { NavLink } from'react-router-dom';
+class TodoList extends Component {
+
+    // This acts as if this.state = {...} was inside constructor()
+    state = {
+        todos: [
+            { id: 1, title: 'SWPP', content: 'take swpp class', done: true},
+            { id: 2, title: 'Movie', content: 'watch movie', done: false },
+            { id: 3, title: 'Dinner', content: 'eat dinner', done: false },
+        ],
+        selectedTodo : null,
+    }
+    clickTodoHandler = td => {
+        if (this.state.selectedTodo === td) {
+            // BAD: this.state.selectedTodo = null; it won't trigger rendering.
+            this.setState({selectedTodo: null});
+        }else {
+            this.setState({selectedTodo: td});
+        }
+    }
+    render() {
+        let todoDetail = null;
+        if (this.state.selectedTodo) {
+            todoDetail = <TodoDetail title={this.state.selectedTodo.title}
+                content={this.state.selectedTodo.content} />
+        }
+        const todos = this.state.todos.map((td) => {
+            return ( <Todo key={td.id} title={td.title}
+                done={td.done} clicked={() => this.clickTodoHandler(td)}/> );
+            });
+        return (
+            <div className = 'TodoList'>
+                <div className='title'>{this.props.title}</div>
+                <div className='todos'>{todos}</div>
+                {todoDetail}
+                <NavLink to='/new-todo'exact>
+                    New Todo
+                </NavLink>
+            </div>
+
+        );  
+    }
+}
+export default TodoList;
